8eeb22b83be3363dc9758b8eb01b76e54dc4f449
Lucas <Lucas@gmail.com>
2022-04-23 06:57:17 -0300

    divisão html / start tela do quiz

--- Javascript/script.js
+++ Javascript/script.js
@@ -4,15 +4,33 @@ function sucessoCriarQuiz() {
 
     document.querySelector(".container-sucesso-quiz").innerHTML =`
         <div>Seu quizz está pronto!</div>
-        <div class="quiz">
-            <img src="${}" alt="Quizz">
-            <span>${}</span>
+        <div class="preQuiz" onclick="acessarQuiz()">
+            <img src="infoQuizz.image" alt="Quizz"/>
+            <span>infoQuizz.title</span>
             <div class="gradiente"></div>
         </div>
         <button onclick="acessarQuiz()">Acessar Quizz</button>
-        <div class="home" onclick="homePage()">Voltar pra home</div>`
+        <div class="voltaHome" onclick="acessarHome()">Voltar pra home</div>`
 
     document.querySelector(".container-sucesso-quiz").classList.remove("desligado");
-    document.querySelector("conteiner-criacao-quiz").classList.add("desligado");
+    document.querySelector(".conteiner-criacao-quiz").classList.add("desligado");
 
+}
+
+function acessarQuiz() {
+
+    document.querySelector(".container-sucesso-quiz").classList.add("desligado");
+    document.querySelector(".quiz").classList.remove("desligado");
+}
+
+function acessarHome () {
+
+    let promise = axios.get('https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes');
+    promise.then(renderizarHome)
+    document.querySelector(".container-sucesso-quiz").classList.add("desligado");
+    document.querySelector(".home").classList.remove("desligado");
+}
+
+function renderizarHome () {
+    
 }
